978a6240198de6e092fb1014c11c0556c6353b99
Dennis Creevey <95920143+HasanGocer@users.noreply.github.com>
2022-06-08 23:40:28 +0300

    Add files via upload

---
+++ C_Odev/ÖDEV1.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <locale.h>
+#include <math.h>
+#include <time.h>
+//sýnavSonuc.txt isimli bir text dosyasý oluþturun.
+//sorunuzun cevaplarýný math kütüphanesini kullanarak cevabýný bulunuz.
+//1. soru:random þekilde 1 ile 10 arasýnda bir sayý isteyin 3 üssünün cevabýný bulun.
+//deðerin üstelli halini sorun. scanf ile cevabý isteyin.
+//cevap:pow() ile cevabý bulun.
+//2. soru:random þekilde 1 ile 100 arasýnda bir sayý isteyin onu -1 ile çarpýn.
+//deðerin mutlak deðerini sorun.scanf ile cevabý isteyin.
+//cevap: onu fabs() ile geri pozitif yapýn.
+//3. soru:random þekilde 1 ile 100 arasýnda iki sayý isteyin onlarý çýkartmasýný isteyin.
+//çýkarma iþlemini sorun ama sonucu mutlak deðerli isteyin. scanf ile cevabý isteyin.
+//cevap:fdim() kullanýn ve cevap mutlak deðerli çýksýn.
+//sonuclarý kontrol edip txte soru sayýsý ve doðru/yanlýþ ýný yazdýrýn.
+
+
+
+int main(){
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
